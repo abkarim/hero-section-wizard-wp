3f06f74eaf4c5a8a84411f5c91c2e9ddbae20d7d
Karim <akskarim17@gmail.com>
2026-07-06 17:13:53 +0600

    added pointer events

--- blocks/hero-section-slider-container/view.js
+++ blocks/hero-section-slider-container/view.js
@@ -3,41 +3,138 @@ document.addEventListener("DOMContentLoaded", () => {
         ".wp-block-hero-section-wizard-hero-section-slider-container",
     );
 
-    if (heroSectionSliderContainer) {
-        const sliderWrapper =
-            heroSectionSliderContainer.querySelector(".slider-wrapper");
+    if (!heroSectionSliderContainer) return;
 
-        const controls = heroSectionSliderContainer.querySelector(".controls");
+    const sliderWrapper =
+        heroSectionSliderContainer.querySelector(".slider-wrapper");
+    const controls = heroSectionSliderContainer.querySelector(".controls");
 
-        if (controls) {
-            controls.addEventListener("click", (event) => {
-                const clickedControl = event.target.closest(".control");
-                const index = Array.from(controls.children).indexOf(
-                    clickedControl,
-                );
+    const totalSlidesIndex = controls ? controls.childNodes.length - 1 : 0;
+    let currentSlideIndex = 0;
 
-                if (index !== -1) {
-                    const activeControl =
-                        controls.querySelector(".control.active");
-                    if (activeControl) {
-                        activeControl.classList.remove("active");
-                    }
+    function canGoToNextSlide() {
+        return currentSlideIndex < totalSlidesIndex;
+    }
 
-                    clickedControl.classList.add("active");
+    function canGoToPreviousSlide() {
+        return currentSlideIndex > 0;
+    }
 
-                    sliderWrapper.setAttribute(
-                        "data-active-slide-index",
-                        index.toString(),
-                    );
-
-                    const transformValue = index < 1 ? 0 : index * 100;
-
-                    heroSectionSliderContainer.style.setProperty(
-                        "--hero-section-slider-container-transform-value",
-                        `translateX(-${transformValue}%)`,
-                    );
-                }
-            });
+    function setDraggingSlideStateTransformValue(value) {
+        console.log({ value });
+        if (value !== "") {
+            heroSectionSliderContainer.style.setProperty(
+                "--hero-section-slider-container-dragging-transform-value",
+                `translateX(${value}%)`,
+            );
+        } else {
+            heroSectionSliderContainer.style.removeProperty(
+                "--hero-section-slider-container-dragging-transform-value",
+            );
         }
     }
+
+    function changeSlide(index, clickedControl) {
+        currentSlideIndex = index;
+        const activeControl = controls.querySelector(".control.active");
+        if (activeControl) {
+            activeControl.classList.remove("active");
+        }
+
+        controls.childNodes[index].classList.add("active");
+
+        sliderWrapper.setAttribute("data-active-slide-index", index.toString());
+
+        const transformValue = index < 1 ? 0 : index * 100;
+
+        heroSectionSliderContainer.style.setProperty(
+            "--hero-section-slider-container-transform-value",
+            `translateX(-${transformValue}%)`,
+        );
+    }
+
+    if (controls) {
+        controls.addEventListener("click", (event) => {
+            const clickedControl = event.target.closest(".control");
+            const index = Array.from(controls.children).indexOf(clickedControl);
+
+            if (index !== -1) changeSlide(index);
+        });
+    }
+
+    let isDragging = false;
+    let startX = 0;
+    let startTime = 0;
+    let slideToBe = null;
+
+    sliderWrapper.addEventListener("pointerdown", (event) => {
+        sliderWrapper.classList.add("dragging");
+        isDragging = true;
+        startX = event.clientX;
+        startTime = event.timeStamp;
+    });
+
+    sliderWrapper.addEventListener("pointermove", (event) => {
+        if (!isDragging) return;
+
+        const currentX = event.clientX;
+        const deltaX = currentX - startX;
+
+        const currentTime = event.timeStamp;
+        const deltaTime = currentTime - startTime;
+
+        const velocityX = deltaX / (deltaTime || 1);
+
+        const direction = deltaX < 0 ? "next" : "previous";
+
+        const sensitivityThreshold = 0.5;
+        const isFastSwipe = Math.abs(velocityX) > sensitivityThreshold;
+
+        let value = 10;
+
+        if (direction === "previous") {
+            if (canGoToPreviousSlide()) {
+                value = `-${parseInt(
+                    currentSlideIndex * 100 -
+                        (Math.abs(deltaX) / sliderWrapper.offsetWidth) * 100,
+                )}`;
+                if (Math.abs(value) < 50 || isFastSwipe) {
+                    slideToBe = currentSlideIndex - 1;
+                }
+            }
+        }
+
+        if (direction === "next") {
+            if (!canGoToNextSlide()) {
+                value = `-${totalSlidesIndex * 100 + value}`;
+            } else {
+                value = `-${parseInt(
+                    currentSlideIndex * 100 +
+                        (Math.abs(deltaX) / sliderWrapper.offsetWidth) * 100,
+                )}`;
+
+                if (Math.abs(value) > 50 || isFastSwipe) {
+                    slideToBe = currentSlideIndex + 1;
+                } else {
+                    slideToBe = null;
+                }
+            }
+        }
+
+        setDraggingSlideStateTransformValue(value);
+    });
+
+    function pointerCancel() {
+        sliderWrapper.classList.remove("dragging");
+        setDraggingSlideStateTransformValue("");
+        isDragging = false;
+
+        if (slideToBe !== null) {
+            changeSlide(slideToBe);
+        }
+    }
+
+    sliderWrapper.addEventListener("pointerup", pointerCancel);
+    sliderWrapper.addEventListener("pointercancel", pointerCancel);
+    sliderWrapper.addEventListener("pointerleave", pointerCancel);
 });
